af6338912bcb73e87f707eccf2f1458b95416db4
anhvd <anhvd@vnpt-technology.vn>
2022-05-18 10:26:31 +0700

    modify index.js

--- front-end/repair-motorbike-shop/src/index.js
+++ front-end/repair-motorbike-shop/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// core components
+import Admin from "layouts/Admin.js";
+
+import "assets/css/material-dashboard-react.css?v=1.9.0";
+import Login from "components/Login/Login";
+
+const hist = createBrowserHistory();
+// const logged = window.sessionStorage.getItem('jwt')
+ReactDOM.render(
+    <Router history={hist}>
+        <Switch>
+            <Route path="/admin" render = {() => {
+                return window.sessionStorage.getItem('jwt') ? <Admin/> : <Redirect to="/login" />
+            }} />
+            <Route path="/login" component={Login} />
+
+            <Redirect from="/" to="/admin/dashboard" />
+        </Switch>
+    </Router>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
