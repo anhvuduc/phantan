e68bd739223cd09e96d1257a00ae343f0b779703
anhvd <anhvd@vnpt-technology.vn>
2022-06-05 16:48:38 +0700

    fix bug snackbar

--- front-end/repair-motorbike-shop/src/components/Snackbar/SnackbarContent.js
+++ front-end/repair-motorbike-shop/src/components/Snackbar/SnackbarContent.js
@@ -1,9 +1,9 @@
 import React from "react";
-import classNames from "classnames";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import Snack from "@material-ui/core/Snackbar";
+import Snack from "@material-ui/core/SnackbarContent";
 import IconButton from "@material-ui/core/IconButton";
 // @material-ui/icons
 import Close from "@material-ui/icons/Close";
@@ -12,9 +12,9 @@ import styles from "assets/jss/material-dashboard-react/components/snackbarConte
 
 const useStyles = makeStyles(styles);
 
-export default function Snackbar(props) {
+export default function SnackbarContent(props) {
   const classes = useStyles();
-  const { message, color, close, icon, place, open, rtlActive } = props;
+  const { message, color, close, icon, rtlActive } = props;
   var action = [];
   const messageClasses = classNames({
     [classes.iconMessage]: icon !== undefined
@@ -26,7 +26,6 @@ export default function Snackbar(props) {
         key="close"
         aria-label="Close"
         color="inherit"
-        onClick={() => props.closeNotification()}
       >
         <Close className={classes.close} />
       </IconButton>
@@ -34,46 +33,26 @@ export default function Snackbar(props) {
   }
   return (
     <Snack
-      anchorOrigin={{
-        vertical: place.indexOf("t") === -1 ? "bottom" : "top",
-        horizontal:
-          place.indexOf("l") !== -1
-            ? "left"
-            : place.indexOf("c") !== -1
-            ? "center"
-            : "right"
-      }}
-      open={open}
       message={
         <div>
           {icon !== undefined ? <props.icon className={classes.icon} /> : null}
           <span className={messageClasses}>{message}</span>
         </div>
       }
-      action={action}
-      ContentProps={{
-        classes: {
-          root: classes.root + " " + classes[color],
-          message: classes.message,
-          action: classNames({ [classes.actionRTL]: rtlActive })
-        }
+      classes={{
+        root: classes.root + " " + classes[color],
+        message: classes.message,
+        action: classNames({ [classes.actionRTL]: rtlActive })
       }}
+      action={action}
     />
   );
 }
 
-Snackbar.propTypes = {
+SnackbarContent.propTypes = {
   message: PropTypes.node.isRequired,
   color: PropTypes.oneOf(["info", "success", "warning", "danger", "primary"]),
   close: PropTypes.bool,
   icon: PropTypes.object,
-  place: PropTypes.oneOf(["tl", "tr", "tc", "br", "bl", "bc"]),
-  open: PropTypes.bool,
-  rtlActive: PropTypes.bool,
-  closeNotification: PropTypes.func
+  rtlActive: PropTypes.bool
 };
-
-// 
-//
-//
-//
