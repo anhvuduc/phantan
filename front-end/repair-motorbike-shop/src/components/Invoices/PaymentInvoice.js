e655b947e6a301a0ae908f439af178fc9f51c646
Vietanh20187291 <62647888+Vietanh20187291@users.noreply.github.com>
2022-06-09 23:41:02 +0700

    fix bug

--- front-end/repair-motorbike-shop/src/components/Invoices/PaymentInvoice.js
+++ front-end/repair-motorbike-shop/src/components/Invoices/PaymentInvoice.js
@@ -20,31 +20,43 @@ function PaymentInvoice(props) {
             }
         };
     });
-      const [tl, setTl] = React.useState(false);
+    
+    const [listCustomerVehicleClass,  setListCustomerVehicleClass] = useState('');
+    const [showInfoCustomerVehicle, setShowInfoCustomerVehicle] = useState('');
+ 
+    const [id, setId] = useState(props.match.params.id);
+    const [showInfoCustomer, setShowInfoCustomer] = useState('');
+    const [showInfoEmployee, setShowInfoEmployee] = useState('');
 
-  const [invoices, setInvoices] = useState([]);
+    const [btnClass, setBtnClass] = useState('');
+    const [materialChoose, setMaterialChoose] = useState([]);
+    const [sumMaterial, setSumMaterial] = useState(0);
 
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+    const [serviceChoose, setServiceChoose] = useState([]);
+    const [sumServices, setSumServices] = useState(0);
+    const [status, setStatus] =  useState('');
 
-  const [pagination, setPagination] = useState({
-    page: 1,
-    limit: 10,
-    totalRows: 10,
-  });
-  const [filters, setFilters] = useState({
-    page: 1,
-    limit: 10,
-    keyword: "",
-    status: [4]
-  });
-  function handlePageChange(newPage) {
-    // console.log("new page: ", newPage);
-    setFilters({
-      ...filters,
-      page: newPage,
+    const [noteInvoice, setNoteInvoice] = useState('');
+    const [payMethod, setPayMethod]  = useState();
+    const [customer, setCustomer] = useState({
+        id: 0,
+        code: '',
+        name: '',
+        phone: '',
     });
-  }
+
+    const [employee, setEmployee] = useState({
+        id: 0,
+        code: '',
+        name: '',
+        phone: ''
+    });
+    const [vehicles, setVehicles] = useState([]);
+    const [vehicle, setVehicle] = useState({
+        id: 0,
+        code: '',
+        licensePlate: ''
+    });
 
     //Lấy thông tin hóa đơn
     useEffect(() => {
