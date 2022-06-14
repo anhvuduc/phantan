23a4f6b3b0ad0bf4ee11d898510ff5031db0abd5
Vietanh20187291 <62647888+Vietanh20187291@users.noreply.github.com>
2022-06-14 14:43:41 +0700

    fix bug

--- front-end/repair-motorbike-shop/src/components/materials/UpdateMaterial.js
+++ front-end/repair-motorbike-shop/src/components/materials/UpdateMaterial.js
@@ -102,113 +102,195 @@ function UpdateMaterial(props) {
   }, []);
 
 
-  const cancel = () => {
-    props.history.push("/admin/materials");
-  };
+    useEffect(() => {
+    async function fetchInvoicesList() {
+      try {
+        console.log("filters: " + filters.status)
+        InvoicesService.getInvoiceInProcess(filters).then((res) => {
+          let invoices = res.data.invoiceMaterialResponseDTOS;
+          const pagination = res.data.pagination;
+          console.log(res.data);
+          setInvoices(
+            invoices.map((invoice) => {
+              return {
+                select: false,
+                id: invoice.id,
+                code: invoice.code,
+                licensePlate: invoice.licensePlate,
+                fixerName: invoice.fixerName,
+                name: invoice.name,
+                phone: invoice.phone,
+                status: invoice.status,
+              }
+            }))
+          setPagination(pagination);
+          console.log(pagination);
+          setIsLoaded(true);
+        }).catch(function (error) {
+          console.log("ERROR: " + error.response.data.status)
+          if (error.response.data.status == 403) {
+            alert("Không có quyền truy cập!")
+          }
+        })
+      } catch (error) {
+        console.log("Failed to fetch Invoicce list: ", error.message);
+        setError(error);
+      }
+    }
+    fetchInvoicesList();
+  }, [filters]);
 
-  return (
-    <div className="body">
-      <Snackbars
-        place="tc"
-        color="warning"
-        message={message}
-        open={tl}
-        closeNotification={() => setTl(false)}
-        close
-      />
-      <div className="title-materials">
-        <div
-          className="button-cancel"
-          onClick={cancel}
-        >
-          <NavigateBefore style={{ width: "15px" }} /> <span>Quay lại</span>
-        </div>
-        <button className="button-add" onClick={updateMaterial}>
-          Lưu
-        </button>
-      </div>
+  const editInvoice = (id) => {
+    props.history.push(`/admin/invoices/edit-invoice-in-process/${id}`)
+  }
 
-      <div className="add-new-material">
-        <div className="title">
-          <span>Sửa thông tin phụ tùng</span>
-        </div>
-        <div className="card-body">
-          <form>
-            <div className="group">
-              <div className="group-main">
-                <div className="form-group">
-                  <label>Tên phụ tùng <span style={{color: 'red'}}> *</span> </label>
-                  <br />
-                  <input
-                    placeholder="Tên phụ tùng"
-                    name="name"
-                    type="text"
-                    className="form-control"
-                    value={name}
-                    onChange={changeName}
-                  />
+  const paymentInvoice = (id) => {
+    props.history.push(`/admin/invoices/payment/${id}`)
+  }
+
+  const paymentInvoiceBuyMaterial = () => {
+    props.history.push('/admin/invoices-buy-material')
+  }
+
+  const back = () => {
+    props.history.push('/admin/invoices');
+  }
+
+  const colorStatusInvoice = status => {
+    if (status.localeCompare("Đã thanh toán") == 0) {
+      return <span style={{ color: "#3c91f1" }}>{status}</span>
+    } else if (status.localeCompare("Chờ thanh toán") == 0) {
+      return <span style={{ color: "#35df24" }}>{status}</span>
+    }
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading....</div>;
+  } else {
+    return (
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <div className="list-invoices-payment">
+            <div className="title-invoices-payment">
+              <div className="name-title">
+
+                <div
+                  className="button-cancel"
+                  onClick={back}
+                >
+                  <NavigateBefore style={{ width: "15px" }} /> <span>Quay lại</span>
                 </div>
-                <div className="form-group">
-                  <label>Mô tả </label>
-                  <br />
-                  <input
-                    placeholder="Thêm mô tả"
-                    name="description"
-                    className="form-control"
-                    value={description}
-                    onChange={changeDescription}
-                  />
+                <div className="name-list"><span>Danh sách phiếu sửa chữa chờ thanh toán</span></div>
+
+              </div>
+            </div>
+
+            <div className="content-invoices-payment">
+              <Snackbars
+                place="tc"
+                color="info"
+                message="Thành công!"
+                open={tl}
+                closeNotification={() => setTl(false)}
+                close
+              />
+              <div className="filter">
+                <FiltersForm onSubmit={handleFiltersChange} />
+                <div className="action">
+                  <div className="select">
+                    <InvoiceFilterPayment onSubmit={handleChangeInvoice} />
+                  </div>
+                  
                 </div>
-                <div className="form-group">
-                  <label>Nhà cung cấp </label>
-                  <br />
-                  <input
-                    placeholder="Điền nhà cung cấp"
-                    name="supplier"
-                    className="form-control"
-                    value={supplier}
-                    onChange={changeSupplier}
-                  />
+                
+
+              </div>
+              <div className="hight-table">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th className="th-1">
+                        <span>#</span>
+                      </th>
+                      <th className="th-2">
+                        <span>Mã phiếu</span>
+                      </th>
+                      <th className="th-3">
+                        <span>Biển số</span>
+                      </th>
+                      <th className="th-4">
+                        <span>Nhân viên</span>
+                      </th>
+                      <th className="th-5">
+                        <span>Tên KH</span>
+                      </th>
+                      <th className="th-5">
+                        <span>Số điện thoại KH</span>
+                      </th>
+                      <th className="th-6">
+                        <span>Trạng thái</span>
+                      </th>
+                      <th className="th-7">
+                        <span></span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {invoices.map((invoice) => (
+                      <tr key={invoice.id}>
+                        <td className="td-1">
+                        <span>{invoice.id}</span>
+                        </td>
+                        <td className="td-2">
+                          <span>{invoice.code}</span>
+                        </td>
+                        <td className="td-3">
+                          <span>{invoice.licensePlate}</span>
+                        </td>
+                        <td className="td-4">
+                          <span>{invoice.fixerName}</span>
+                        </td>
+                        <td className="td-5">
+                          <span>{invoice.name}</span>
+                        </td>
+                        <td className="td-6">
+                          <span>{invoice.phone}</span>
+                        </td>
+                        <td className="td-6">
+                          {colorStatusInvoice(invoice.status)}
+                        </td>
+                        <td className="td-7">
+                          <button
+                            className="button-icon"
+                            onClick={() => paymentInvoice(invoice.id)}
+                          >
+                            <PaymentIcon style={{ width: "15px" }} /><div className="info-button"><span>Thanh Toán</span></div>
+                          </button>
+                        </td>
+                        <td></td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="pagination-limit">
+                <div className="limit">
+                  <span>Hiển thị </span><LimitPagination onSubmit={handleChangeLimit} /> <span style={{ marginTop: "21px" }}> kết quả</span>
                 </div>
-                <div className="form-group">
-                  <label>Số lượng </label>
-                  <br />
-                  <input
-                    placeholder="Điền số lượng"
-                    name="quantity"
-                    className="form-control"
-                    value={quantity}
-                    disabled
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Giá nhập <span style={{color: 'red'}}> *</span> </label>
-                  <br />
-                  <input
-                    placeholder="Điền giá nhập vào"
-                    name="inputPrice"
-                    className="form-control"
-                    value={inputPrice}
-                    onChange={changeInputPrice}
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Giá bán <span style={{color: 'red'}}> *</span> </label>
-                  <br />
-                  <input
-                    placeholder="Điền giá bán ra"
-                    name="outputPrice"
-                    className="form-control"
-                    value={outputPrice}
-                    onChange={changeOutputPrice}
+                <div className="pagination">
+                  <Pagination
+                    pagination={pagination}
+                    onPageChange={handlePageChange}
                   />
                 </div>
               </div>
             </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+          </div>
+        </GridItem>
+
+      </GridContainer>
+    );
+  }
 }
-export default UpdateMaterial;
